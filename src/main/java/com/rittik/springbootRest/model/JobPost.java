90b545574d745020dbd818b9cba4123605975b94
Rittik Ram <hrittickramspi40@gmail.com>
2025-06-21 01:40:43 +0530

    Initial commit - Job API Server

---
+++ src/main/java/com/rittik/springbootRest/model/JobPost.java
@@ -0,0 +1,30 @@
+package com.rittik.springbootRest.model;
+
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+public class JobPost {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postId;
+	private String postProfile; 
+	private String postDesc;
+	private Integer reqExperience;
+	private List<String> postTechStack;
+	
+
+}
